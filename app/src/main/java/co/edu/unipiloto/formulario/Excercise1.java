2fb246b5f36794d58587cc2bbe3d4d76f807f6bb
Yohan <yohan-garzon@upc.edu.co>
2024-03-02 17:42:36 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/formulario/Excercise1.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.formulario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Excercise1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excercise1);
+    }
+}
---
+++ app/src/main/java/co/edu/unipiloto/formulario/Excercise4.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.formulario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Excercise4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excercise4);
+    }
+}
